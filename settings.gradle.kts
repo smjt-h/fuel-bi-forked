f11863869c5849561ec02d383dc9837c1d6348d8
architmallik7 <165177188+architmallik7@users.noreply.github.com>
2024-07-30 18:20:48 +0530

    Added BI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,20 @@
+pluginManagement {
+    includeBuild("plugins")
+}
+
+// import the plugin
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("io.harness:gradle-cache:0.0.2")
+    }
+}
+
+// apply the plugin
+apply(plugin = "io.harness.gradle-cache")
+
 rootProject.name = "Fuel-MPP"
 
 include(":fuel")
@@ -9,6 +26,19 @@ include(":fuel-moshi-jvm")
 include(":samples:httpbin-wasm")
 include(":samples:mockbin-native")
 
-pluginManagement {
-    includeBuild("plugins")
+// build cache config
+buildCache {
+    local {
+        // Local build cache is dangerous as it might produce inconsistent results
+        // in case developer modifies files while the build is running
+        isEnabled = false
+    }
+    remote(io.harness.Cache::class.java) {
+        accountId = System.getenv("HARNESS_ACCOUNT_ID") // accountId should be populated in CI pipeline
+        token = System.getenv("HARNESS_PAT")            // API token with account admin (or edit) permissions
+        isPush = true
+        endpoint = System.getenv("HARNESS_CACHE_SERVICE_ENDPOINT") // https://app.harness.io/gateway/cache-service
+    }
 }
+
+# Test
